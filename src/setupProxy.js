b1dd2f6872a155a0ecc9c59b378e6baaf7361817
user3035 <1536844110@qq.com>
2022-10-11 15:38:44 +0800

    push

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware')
 module.exports = function(app) {
     app.use(
         proxy.createProxyMiddleware('/api1', {  //api1是需要转发的请求(所有带有/api1前缀的请求都会转发给5000)
-            // target: 'http://ceshi13.dishait.cn', //配置转发目标地址(能返回数据的服务器地址)
+            target: 'http://ceshi13.dishait.cn', //配置转发目标地址(能返回数据的服务器地址)
             changeOrigin: true, //控制服务器接收到的请求头中host字段的值
             /*
                 changeOrigin设置为true时，服务器收到的请求头中的host为：localhost:5000
